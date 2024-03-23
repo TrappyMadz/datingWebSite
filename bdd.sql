b0c41f8e5ca7b661545478949b965f0894fc36aa
HappyGood0 <163296899+HappyGood0@users.noreply.github.com>
2024-03-23 22:45:57 +0100

    Update bdd.sql
    correction bdd

--- bdd.sql
+++ bdd.sql
@@ -1,11 +1,17 @@
-CREATE DATABASE bdd;
 USE bdd;
 
-CREATE TABLE utilisateurs (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nom_utilisateur VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    mot_de_passe VARCHAR(255) NOT NULL
-);
 
-SELECT * FROM utilisateurs;
+CREATE TABLE utilisateurs(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    prenom VARCHAR(255) NOT NULL,
+    pseudo VARCHAR(255) NOT NULL,
+    nom VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    mot_de_passe VARCHAR(255) NOT NULL,
+    statut VARCHAR(255) NOT NULL,
+    adresse VARCHAR(255) NOT NULL,
+    ville VARCHAR(255) NOT NULL,
+    lien VARCHAR(10000) NOT NULL
+ 
+);
+SELECT * FROM utilisateurs; 
