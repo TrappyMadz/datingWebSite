e6f1cdd418b6f98af76eacee3a8ef6ebdd401b60
Voidhi <163297654+Voidhi@users.noreply.github.com>
2024-03-26 13:34:00 +0100

    Update abonnements.php

--- abonnements.php
+++ abonnements.php
@@ -1,9 +1,23 @@
 <?php
 session_start();
+include 'nonAccessiblePhpPages/bdd.php';
 if (!isset($_SESSION['username'])) {
     header("Location: connexion.php");
     exit();
 }
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_SESSION['username'];
+    $sql = "UPDATE utilisateurs SET statut = 'abonne' WHERE pseudo = '$username'";
+    
+    // Exécuter la requête SQL
+    if ($conn->query($sql) === TRUE) {
+        echo "Abonnement réussi !";
+    } else {
+        echo "Erreur lors de la mise à jour du statut : " . $conn->error;
+    }
+}
+
 ?>
 
 
@@ -13,109 +27,58 @@ if (!isset($_SESSION['username'])) {
 	<title>Pistons & Passions</title>
 	<!-- Pour l'icone de l'onglet : -->
 	<link rel="shortcut icon" href="img/logo.png" />
-	<link rel="stylesheet" type="text/css" href="styleAbo.css" />
+	<link rel="stylesheet" type="text/css" href="css/styleAbo.css" />
 	<meta name="author" content="LAKOMICKI ROBLES CHARRIER CARRIAC" />
 	<meta charset="utf-8">
 </head>
 
 <body>
-    <header> <!-- Le menu du haut :  -->
-            <!-- le logo (renvoi sur le menu en cliquant) -->
-            <a href="accueil.php" title="Menu">
-                <img id="logoHeader" alt="logo" src="img/logo.png" width="80em">
-            </a>
-            <h2> Pistons & Passions </h1>
-        <nav class="NavMenu">
-            <a href="accueil.php">MENU</a>
-            <a href="abonnements.php">ABONNEMENTS</a>
-            <a href="profil.php">MON PROFIL</a>
-        </nav>
-            <a href="messagerie.php">
-                <img id="logoMess" alt="Messagerie" src="img/envelope.png" width="45em">
-            </a>
-            <?php
-            session_start();
-            if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
-                echo '<a href="deconnexion.php" class="bouton">DECONNEXION</a>';
-            } else {
-                echo '<a href="connexion.php" class="bouton">CONNEXION</a>';
-            }
-            ?>
-    </header>
+    
+    <?php
+        // Menu :
+        include 'nonAccessiblePhpPages/header.php';
+    ?>
+    
+
     <h1> Abonnez-vous et gagnez de nombreux avantages ! </h1>
     <p> Envoyez des messages à vos coup de moteur</p>
     <p> Soyez au courant des voitures qui sont passées sur votre profil</p>
     <div class="Page_Principale">
         <br>
         <div id="cote">
-        <div class="row">
-            <div class="col-75">
-                <div class="container">
-                    <form action="/action_page.php">
-                        <div class="row">
-                            <div class="col-50">
-                                <div class="radio-toolbar">
-                                    <input type="radio" id="radioTarif1" name="tarif" value="Tarif1" checked>
-                                    <label for="radioTarif1">1 mois<br>5€</label>
+            <div class="container">
+                <br>
+                <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+                    <div class="radio-toolbar">
+                        <input type="radio" id="radioTarif1" name="tarif" value="Tarif1" checked>
+                        <label for="radioTarif1">1 mois<br>5€</label>
 
-                                    <input type="radio" id="radioTarif2" name="tarif" value="Tarif2">
-                                    <label for="radioTarif2">3 mois<br>13€</label>
+                        <input type="radio" id="radioTarif2" name="tarif" value="Tarif2">
+                        <label for="radioTarif2">3 mois<br>13€</label>
 
-                                    <input type="radio" id="radioTarif3" name="tarif" value="Tarif3">
-                                    <label for="radioTarif3">1 an<br>50€</label>
-
-                                </div>
-                                <h3>Adresse de paiement</h3>
-                                <label for="Fname">Nom complet</label>
-                                <input type="text" id="fname" name="firstname" required>
-                                <label for="email">Email</label>
-                                <input type="text" id="email" name="email" required>
-                                <label for="adr">Addresse</label>
-                                <input type="text" id="adr" name="address" required>
-                                <label for="city">Ville</label>
-                                <input type="text" id="city" name="city" required>
-                                <div class="row">
-                                    <div class="col-50">
-                                        <label for="departement">Département</label>
-                                        <input type="text" id="departement" name="departement" required>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-50">
-                                <h3>Paiement</h3>
-                                <label for="fname">Cartes acceptées</label>
-                                <div class="icon-container">
-                                    <img class="card" src="./img/mastercard.png" alt="Master">
-                                    <img class="card" src="./img/bluecard.png" alt="Blue">
-                                    <img class="card" src="./img/redcard.png" alt="Red">
-                                    <img class="card" src="./img/visa.png" alt="Visa">
-                                </div>
-                                <label for="cname">Nom sur la carte</label>
-                                <input type="text" id="cname" name="cardname" required>
-                                <label for="ccnum">Numéro de carte</label>
-                                <input type="text" id="ccnum" name="cardnumber" required>
-                                <label for="expmonth">Mois d'expiration</label>
-                                <input type="text" id="expmonth" name="expmonth" required>
-                                <div class="row">
-                                    <div class="col-50">
-                                        <label for="expyear">Année d'expiration</label>
-                                        <input type="text" id="expyear" name="expyear" required>
-                                    </div>
-                                    <div class="col-50">
-                                        <label for="cvv">CVV</label>
-                                        <input type="text" id="cvv" name="cvv" required>
-                                     </div>
-                                </div>
-                            </div>
-
-                        </div>
-                        <input type="submit" value="Payer" class="btn">
-                    </form>
-                </div>
+                        <input type="radio" id="radioTarif3" name="tarif" value="Tarif3">
+                        <label for="radioTarif3">1 an<br>50€</label>
+                    </div>
+                    <h3>Paiement</h3>
+                    <label for="fname">Cartes acceptées</label>
+                    <div class="icon-container">
+                        <img class="card" src="./img/mastercard.png" alt="Master">
+                        <img class="card" src="./img/bluecard.png" alt="Blue">
+                        <img class="card" src="./img/redcard.png" alt="Red">
+                        <img class="card" src="./img/visa.png" alt="Visa">
+                    </div>
+                    <label for="cname">Nom sur la carte</label>
+                    <input type="text" id="cname" name="cardname" required>
+                    <label for="ccnum">Numéro de carte</label>
+                    <input type="number" id="ccnum" name="cardnumber" required>
+                    <label for="exp">Date expiration</label>
+                    <input type="month" id="exp" name="exp" required>
+                    <label for="cvv">CVV</label>
+                    <input type="number" id="cvv" name="cvv" required>
+                    <input type="submit" value="Payer" class="btn">
+                </form>
             </div>
         </div>
-    </div>
     </div>
 </body>
 </html>
